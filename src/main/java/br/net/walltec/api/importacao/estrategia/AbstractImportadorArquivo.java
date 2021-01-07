b6d870a32a6ff9643b839a24409cb387f8fd5284
Wallace Rodrigo <wallacerodrigon@gmail.com>
2021-01-06 23:04:24 -0300

    conciliacao da CEF

---
+++ src/main/java/br/net/walltec/api/importacao/estrategia/AbstractImportadorArquivo.java
@@ -0,0 +1,69 @@
+package br.net.walltec.api.importacao.estrategia;
+
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.enterprise.inject.spi.CDI;
+
+import br.net.walltec.api.comum.PageRequest;
+import br.net.walltec.api.comum.PageResponse;
+import br.net.walltec.api.entidades.Banco;
+import br.net.walltec.api.entidades.DeparaHistoricoBanco;
+import br.net.walltec.api.entidades.Lancamento;
+import br.net.walltec.api.excecoes.NegocioException;
+import br.net.walltec.api.negocio.servicos.BancoService;
+import br.net.walltec.api.negocio.servicos.DeparaHistoricoBancoService;
+
+public abstract class AbstractImportadorArquivo implements ImportadorArquivo {
+
+	private DeparaHistoricoBancoService deparaServico;
+	
+	private BancoService bancoService;
+	
+	/**
+	 * @param string
+	 * @param listaDeparas
+	 * @return
+	 */
+	protected DeparaHistoricoBanco recuperarDepara(String nomeExtrato, List<DeparaHistoricoBanco> listaDeparas) {
+		return listaDeparas
+				.stream()
+				.filter(depara -> nomeExtrato.toLowerCase().contains(depara.getNomeOrigem().toLowerCase()))
+				.findFirst()
+				.orElse(null);
+	}
+	
+	public boolean isExtensaoValida(String extensao) {
+		return extensao.equalsIgnoreCase("txt");
+	}
+	
+	/**
+	 * @param dadosArquivo
+	 * @param listaDeparas 
+	 */
+	private List<Lancamento> recuperarListaLancamento(byte[] dadosArquivo) throws NegocioException {
+		String[] dados = new String(dadosArquivo, Charset.forName(getCharset())).split("\n");
+		
+		bancoService =  CDI.current().select(BancoService.class).get();
+		Banco banco = bancoService.find(getNumeroBanco());
+
+		deparaServico =  CDI.current().select(DeparaHistoricoBancoService.class).get();
+		PageResponse<List<DeparaHistoricoBanco>> response = this.deparaServico.listar(new PageRequest(0, 1000000));
+		List<DeparaHistoricoBanco> listaDeparas = response.getResultado().stream().filter(depara -> depara.getBanco().getNumBanco().equals(getNumeroBanco())).collect(Collectors.toList());
+		
+		return extrairDadosParaLancamentos(dados, banco, listaDeparas);
+	}
+	
+	protected abstract Integer getNumeroBanco();
+
+	protected abstract String getCharset();
+	
+	protected abstract List<Lancamento> extrairDadosParaLancamentos(String[] dados, Banco banco, List<DeparaHistoricoBanco> listaDeparas);
+
+	@Override
+	public List<Lancamento> importar(String nomeArquivo, byte[] dadosArquivo) throws NegocioException {
+		return this.recuperarListaLancamento(dadosArquivo);	
+	}
+	
+}
--- src/main/java/br/net/walltec/api/importacao/estrategia/ImportadorCSVBB.java
+++ src/main/java/br/net/walltec/api/importacao/estrategia/ImportadorCSVBB.java
@@ -1,50 +1,25 @@
 package br.net.walltec.api.importacao.estrategia;
 
 import java.math.BigDecimal;
-import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import javax.enterprise.inject.spi.CDI;
-
-import br.net.walltec.api.comum.PageRequest;
-import br.net.walltec.api.comum.PageResponse;
-import br.net.walltec.api.comum.Pageable;
 import br.net.walltec.api.entidades.Banco;
 import br.net.walltec.api.entidades.DeparaHistoricoBanco;
-import br.net.walltec.api.entidades.FormaPagamento;
 import br.net.walltec.api.entidades.Lancamento;
 import br.net.walltec.api.entidades.TipoLancamento;
-import br.net.walltec.api.excecoes.NegocioException;
-import br.net.walltec.api.negocio.servicos.BancoService;
-import br.net.walltec.api.negocio.servicos.DeparaHistoricoBancoService;
-import br.net.walltec.api.negocio.servicos.TipoLancamentoService;
 import br.net.walltec.api.utilitarios.UtilData;
 
-public class ImportadorCSVBB implements ImportadorArquivo {
+public class ImportadorCSVBB extends AbstractImportadorArquivo {
 
 	public static final String DATA = "dd/MM/yyyy";	
 	private static final String CHARSET_8859_1 = "ISO-8859-1";	
 	private static final int NUM_BB = 1;
 
-	private DeparaHistoricoBancoService deparaServico;
-	
-	private BancoService bancoService;
-	
 
-	/* (non-Javadoc)
-	 * @see br.net.walltec.api.importacao.estrategia.ImportadorArquivo#importar(java.lang.String, byte[], java.util.List)
-	 */
-	@Override
-	public List<Lancamento> importar(String nomeArquivo, byte[] dadosArquivo)
-			throws NegocioException {
 	
-		List<Lancamento> lancamentosConvertidos = this.recuperarListaLancamento(dadosArquivo);
-		return lancamentosConvertidos;
-	}
 	
 	/**
 	 * @return Retorna a data no formato dd/mm/yyyy.
@@ -52,22 +27,7 @@ public class ImportadorCSVBB implements ImportadorArquivo {
 	public static String getDataFormatoString(Date data) {
 		return data==null ? null : new SimpleDateFormat(DATA).format(data);
 	}
-	/**
-	 * @param dadosArquivo
-	 * @param listaDeparas 
-	 */
-	private List<Lancamento> recuperarListaLancamento(byte[] dadosArquivo) throws NegocioException {
-		String[] dados = new String(dadosArquivo, Charset.forName(CHARSET_8859_1)).split("\n");
-		
-		bancoService =  CDI.current().select(BancoService.class).get();
-		Banco banco = bancoService.find(NUM_BB);
-
-		deparaServico =  CDI.current().select(DeparaHistoricoBancoService.class).get();
-		PageResponse<List<DeparaHistoricoBanco>> response = this.deparaServico.listar(new PageRequest(0, 1000000));
-		List<DeparaHistoricoBanco> listaDeparas = response.getResultado().stream().filter(depara -> depara.getBanco().getNumBanco().equals(NUM_BB)).collect(Collectors.toList());
-		
-		return extrairDadosParaLancamentos(dados, banco, listaDeparas);
-	}
+	
 
 	/**
 	 * @param dados
@@ -77,7 +37,8 @@ public class ImportadorCSVBB implements ImportadorArquivo {
 	 * @param tipoLancamento
 	 * @param lancamentos
 	 */
-	private List<Lancamento> extrairDadosParaLancamentos(String[] dados, Banco banco, List<DeparaHistoricoBanco> listaDeparas) {
+	@Override
+	protected List<Lancamento> extrairDadosParaLancamentos(String[] dados, Banco banco, List<DeparaHistoricoBanco> listaDeparas) {
 		int numLinha = 0;
 		List<Lancamento> lancamentos = new ArrayList<Lancamento>();
 		for(String dado : dados) {
@@ -128,22 +89,25 @@ public class ImportadorCSVBB implements ImportadorArquivo {
 		return lancamentos;
 	}
 //
-	/**
-	 * @param string
-	 * @param listaDeparas
-	 * @return
-	 */
-	private DeparaHistoricoBanco recuperarDepara(String nomeExtrato, List<DeparaHistoricoBanco> listaDeparas) {
-		return listaDeparas
-				.stream()
-				.filter(depara -> nomeExtrato.toLowerCase().contains(depara.getNomeOrigem().toLowerCase()))
-				.findFirst()
-				.orElse(null);
-	}
+	
 
 	@Override
 	public boolean isExtensaoValida(String extensao) {
 		return extensao.equalsIgnoreCase("csv");
+	}
+
+
+	@Override
+	protected Integer getNumeroBanco() {
+		// TODO Auto-generated method stub
+		return NUM_BB;
+	}
+
+
+	@Override
+	protected String getCharset() {
+		// TODO Auto-generated method stub
+		return CHARSET_8859_1;
 	}
 	
 }
