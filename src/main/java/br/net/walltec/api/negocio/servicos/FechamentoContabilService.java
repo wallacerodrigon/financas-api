91c6b49ea396f89b0c37880cb526866286050003
Wallace Rodrigo <wallacerodrigon@gmail.com>
2021-01-31 18:52:43 -0300

    Implementado fechamento por usuario

--- src/main/java/br/net/walltec/api/negocio/servicos/FechamentoContabilService.java
+++ src/main/java/br/net/walltec/api/negocio/servicos/FechamentoContabilService.java
@@ -5,6 +5,7 @@ package br.net.walltec.api.negocio.servicos;
 
 
 import br.net.walltec.api.entidades.FechamentoContabil;
+import br.net.walltec.api.entidades.Usuario;
 import br.net.walltec.api.excecoes.NegocioException;
 import br.net.walltec.api.negocio.servicos.comum.CrudPadraoService;
 
@@ -14,6 +15,6 @@ import br.net.walltec.api.negocio.servicos.comum.CrudPadraoService;
  */
 public interface FechamentoContabilService extends CrudPadraoService<FechamentoContabil> {
 	
-	FechamentoContabil obterPorMesAno(Integer ano, Integer mes) throws NegocioException;
+	FechamentoContabil obterPorMesAno(Integer ano, Integer mes, Usuario usuario) throws NegocioException;
 	
 }
